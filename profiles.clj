d390ab2a10cc252382437125da11f300de6edb47
Yuanbo Han <yuanbo.han@bearyinnovative.com>
2016-06-04 18:34:29 +0800

    fix clj-refactor version in profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,3 @@
 {:user {:plugins [[cider/cider-nrepl "0.13.0-snapshot"] ;; http://cider.readthedocs.io
-                  [refactor-nrepl "2.2.0"]]
+                  [refactor-nrepl "2.3.0-SNAPSHOT"]]
         :dependencies [[acyclic/squiggly-clojure "0.1.5"]]}}
