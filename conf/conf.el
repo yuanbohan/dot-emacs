fbd372cdf212537d661966d55fdf5bf986375ffa
Yuanbo Han <yuanbo.han@bearyinnovative.com>
2016-06-05 09:19:47 +0800

    change pairs

--- conf/conf.el
+++ conf/conf.el
@@ -190,10 +190,8 @@
 (sp-pair "(" ")"   :wrap "C-c (")
 (sp-pair "[" "]"   :wrap "C-c [")
 (sp-pair "{" "}"   :wrap "C-c {")
-(sp-pair "*" "*"   :wrap "C-c *")
 (sp-pair "'" "'"   :wrap "C-c '")
 (sp-pair "\"" "\"" :wrap "C-c \"")
-(sp-pair "_" "_"   :wrap "C-c _")
 (sp-pair "`" "`"   :wrap "C-c `")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;; ace-jump-mode ;;;;;;;;;;;;;;;;;;;;;;;;;;;
